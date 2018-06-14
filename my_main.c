4b6ec4f08dce523846489d2da245fc2f8795a22c
Edward Ro <ero@stuy.edu>
2018-06-14 08:55:29 -0400

    other meshes

--- my_main.c
+++ my_main.c
@@ -203,7 +203,7 @@ void my_main() {
   g.red = 0;
   g.green = 0;
   g.blue = 0;
-  double step_3d = 30;
+  double step_3d = 10;
   double theta;
   double knob_value, xval, yval, zval;
   SYMTAB * sym;
